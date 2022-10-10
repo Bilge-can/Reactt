7d12fa59e4e32aa71dbdad4b048c883cc45afb1b
Bilge-can <bilgecan618@gmail.com>
2022-10-10 18:19:21 +0300

    react

--- react-test/src/callback.js
+++ react-test/src/callback.js
@@ -14,6 +14,52 @@
 //    console.log("bilge")
 //});
 
-fetch("https://jsonplaceholder.typicode.com/users").then((data) =>
-console.log(data)
-);
+//fetch("https://jsonplaceholder.typicode.com/users")
+//    .then((data) => data.json())
+//    .then((users) => {
+//        console.log("Users Yuklendi!", users);
+
+
+//fetch("https://jsonplaceholder.typicode.com/posts/1")
+//    .then((data) => data.json())
+//    .then((post) => {
+//        console.log("Post Yuklendi!", post);
+
+
+//fetch("https://jsonplaceholder.typicode.com/posts/2")
+//    .then((data) => data.json())
+//.then((data) => data.json())
+//.then((data)=> console.log("Post2 yüklendi!", data));
+//    });
+//    });
+
+//async function getData(){
+//    const users = await(
+//      await  fetch("https://jsonplaceholder.typicode.com/users")
+//    ).json();
+
+//    const post1 = await(
+//        await  fetch("https://jsonplaceholder.typicode.com/posts/1")
+//        ).json();
+
+//    const post2 = await(
+//        await  fetch("https://jsonplaceholder.typicode.com/posts/2")
+//    ).json();
+
+//    console.log("users",users);
+//    console.log("post1",post1);
+//    console.log("post2",post2);
+//}
+//getData();
+
+(async () => {
+    const users = await axios("https://jsonplaceholder.typicode.com/users");
+
+    const post1 = await axios("https://jsonplaceholder.typicode.com/posts/1");
+
+    const post2 = await axios("https://jsonplaceholder.typicode.com/posts/2");
+
+    console.log("users",users);
+    console.log("post1",post1);
+    console.log("post2",post2);
+})();
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,3 +1,4 @@
 import topla from "./my-module.js"
-import "./callback"
+import "./callback.js"
+
 console.log(topla(2, 7));
