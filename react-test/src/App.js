fb96679ee813a87ffe9339c614a5acb79cc509d8
Bilge-can <bilgecan618@gmail.com>
2022-10-11 15:43:59 +0300

    react

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./companents/Header";
+import User from "./companents/User";
+
+//const name = "Bilge";
+//const surname ="Can";
+//const isLoggedIn = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Header />
+          <p className="xyz">
+            sdfghjklşslkdjldsjhfkfjhgjkdhkjfhglkjdfhgkjdfhgkdjhfgkjhkjh
+              fjhsdkjfhsldjfhldsjkhfslkdjfhsdljhflsdkjfhldsjfhsjdfhslkdhdfj
+              sdkjhfsdfhhldfsjkdhfsldjhflskdjfsldjfhslkjdhflsjdflskdjfhskdljh
+          </p>
+
+          <label htmlFor ="myinput">
+            Name
+              <input id ="myinput"/>
+          </label>
+      </div>
   );
 }
 
 export default App;
+
+//function App(){
+//    return (
+//        <div>
+//            <h1>{`Benim adım ${name}, soyadım ${surname}.`}</h1>
+//            <h1>
+//                {
+//                    isLoggedIn
+//                    ?`Benim adım ${name}, soyadım ${surname}.`
+//                    :"Giriş yapmadınız."
+//                }
+//            </h1>
+//        </div>
+//    )
+//}
+//export default App;
--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,4 +1,5 @@
 import topla from "./my-module.js"
 import "./callback.js"
 import "./array-function"
+import "./App"
 console.log(topla(2, 7));
